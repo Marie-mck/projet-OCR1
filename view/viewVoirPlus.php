60b8a7163046443bed91967f33358ab2bba5b3e9
Marie <marie.cmk22@gmail.com>
2020-12-03 11:00:48 +0100

    ajout signalement des comentaires

--- view/viewVoirPlus.php
+++ view/viewVoirPlus.php
@@ -24,35 +24,40 @@
             <p class="addCommentLink"><a class="addCommentBtn" href="index.php?action=ajoutComment&id=<?php echo $post->id()?>">Pour ajouter un commentaire</a></p>
         </div>
 
+        <div id="commentList">
         <h3 class="titleVoirPlus">Liste des Commentaires pour ce chapitre</h3>
         <?php
         while ($commentaire = $commentaires->fetch()) {
         ?>
-            <p><strong><?= htmlspecialchars($commentaire['authorComment']) ?></strong> le <?= $commentaire['dateComment'] ?></p>
-            <p><?= nl2br(htmlspecialchars($commentaire['commentaire'])) ?></p>
+            <div class="containerComment">
+                <p class="commentTitle"><strong><?= htmlspecialchars($commentaire['authorComment']) ?></strong> le <?= $commentaire['dateComment'] ?></p>
+                <p><?= nl2br(htmlspecialchars($commentaire['commentaire'])) ?></p>
+                <button type="signaler" name="signaler" id="signalerBouton" value="signaler">
+                    <a class="signalerBtn" href="index.php?action=signalerComment">Signaler le commentaire</a></button>
+        </div>
+            
         <?php
         }
         ?>
+        </div>
 
-        <div id="commentList">
-
-        <h3 class="titleVoirPlus">Liste des Commentaires pour ce chapitre</h3>
-            <?php foreach ($commentaires as $commentaire) { ?>
+        <!--- <h3 class="titleVoirPlus">Liste des Commentaires pour ce chapitre</h3>
+            <?php //foreach ($commentaires as $commentaire) { ?>
                 <div class="lastDoc">
-                    <div class="titleDoc">
+                    <div class="titleCommentList">
                         <h4>
-                            <em>publier par<?php echo htmlspecialchars($commentaire->authorComment()); ?></em>
-                            <em>- le <?php echo $commentaire->dateComment(); ?></em>
+                            <em>publier par<?php //echo htmlspecialchars($commentaire->authorComment()); ?></em>
+                            <em>- le <?php // echo $commentaire->dateComment(); ?></em>
                         </h4>
                     </div>
-                    <div class="textDoc">
-                        <?php echo nl2br(htmlspecialchars($commentaire->commentaire())); ?>
+                    <div class="textCommentList">
+                        <?php //echo nl2br(htmlspecialchars($commentaire->commentaire())); ?>
                     </div>
                     <div>
                         <p class="signaler"><a class="signaler" href="index.php?action=signaler&id=<?php //echo $post->id()?>">Pour signaler un commentaire</a></p>
                     </div>
                 </div>
-            <?php } ?>
-        </div>
+            <?php// } ?>
+        --->
 
     </section>
